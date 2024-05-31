e464a5388cd5d74b85e93099336bf3093875be1d
Rajkumar <163848085+JKGlobalItSolutions@users.noreply.github.com>
2024-05-31 13:13:34 +0530

    today changes
    all pages navbar and footetrt section and content are added

--- js/new.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("back-to-top").addEventListener("click", function() {
-    // Scroll to the top of the page smoothly
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
