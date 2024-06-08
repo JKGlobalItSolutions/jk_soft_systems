7ca50e53143ac3746b01dafeb343704209875552
Rajkumar <163848085+JKGlobalItSolutions@users.noreply.github.com>
2024-06-08 16:42:17 +0530

    today changes

---
+++ images/contact us image_files/color.php
@@ -0,0 +1,182 @@
+/*========== Color ====================*/
+
+
+.main-menu .navigation > li:hover > a, .main-menu .navigation > li.current > a,
+.main-menu .navigation > li > ul > li:hover > a,
+.header-top-one .info li a:hover,
+.header-top-one .language .lang-dropdown li a:hover,
+.list-style-one li:before,
+.about-section .year-experience .text,
+.service-block .more-link a,
+.service-block-two h3 a:hover,
+.news-block h3 a:hover, .news-block .lower-box .meta-info li a:hover,
+.news-block .lower-box .meta-info li a:hover,
+.news-block .more-link a,
+.main-footer .links-widget .links li a:hover,
+.main-footer .links-widget .links li:before,
+.main-footer .social-links li a:hover,
+.main-footer .footer-bottom .copyright a,
+.service-block-three h3 a:hover, .service-block-three .inner-box:hover h3 a,
+.service-block-three .more-link a,
+.feature-block .inner-box:hover .icon-box,
+.case-block-two .content-box .link-box a,
+.case-block-two .content-box .link-box a:hover .arrow,
+.team-block .title-box .designation,
+.bkr003 .testimonial-8 .testimonial-item-one .text-area span,
+.featured-block .more-link a,
+.case-block-two .content-box .title-box h3 a:hover,
+.service-block-four h3 a:hover, .service-block-four .inner-box:hover h3 a,
+.service-block-four:hover .icon-outer .icon-box,
+.sidebar .downloads-widget li a:hover,
+.case-single-section .case-inner .cases-title .info-column .inner h3,
+.news-block-three h3 a:hover, .news-block-three .lower-box .meta-info li a:hover,
+.news-block-three .more-link a,
+.widget_categories ul li a:hover,
+.post-details .lower-row .tags a:hover,
+.video-section .video-link a,
+.testimonials-section-ex .slide-item .icon-box,
+.bkr003 .pricing-6 .btn-style-one:hover,
+.service-ex-block-two .inner-box .lower-content .icon-box,
+.service-ex-block-two .inner-box .lower-content .text h4 a:hover,
+.service-ex-block-two .inner-box .lower-content .text .link a:hover,
+.team-ex-block-two .inner-box .content-box .social-links li a,
+.news-ex-block .inner-box .lower-content h4 a:hover,
+.news-ex-block .inner-box .lower-content .post-info li a:hover,
+.news-ex-block .inner-box .lower-content .link a:hover,
+.bkr003 .pricing-5 .single-pricing-plan .list li span:before,
+.bkr003 .pricing-5 .top-title,
+.tt-coupons .tt-right-top .tt-title__02,
+.tt-coupons .tt-right-bottom .tt-text .tt-base-color,
+.bkr003 .team-1 .team-block-two .lower-box .name a:hover,
+.bkr003 .team-1 .team-block-two .lower-box .designation,
+.bkr003 .team-1 .social-icon-two li a:hover,
+.widget ul li a:hover,
+.recent-posts .sidebar .text a:hover,
+.recent-posts .sidebar .meta-info li a:hover,
+.comments-area .comment-box .reply-btn a .fa,
+.comments-area .comment-box .reply-btn a:hover,
+.main-header .header-upper .info .info-block .icon-box,
+.main-header .header-upper .info a:hover, .main-header .header-upper .info a:hover strong,
+.cases-section-ex .case-block .content-box .link-box a,
+.cases-section-ex .cases-carousel .owl-nav .owl-prev, .cases-section-ex .cases-carousel .owl-nav .owl-next,
+.pricing-block-one .pricing-table .table-footer a:hover,
+.featured-block h3 a:hover,
+.cases-section-ex .case-block .content-box .link-box a
+
+{
+	color:#2b7f36!important;
+}
+
+/*==============================================
+   Theme Background Css
+===============================================*/
+.woocommerce #place_order, .woocommerce #respond input#submit, .woocommerce a.button, .woocommerce button.button, .woocommerce input.button,.btn-style-one .btn-title,
+.scroll-to-top,
+.service-block .inner-box .more-link a:hover,
+.services-section .bottom-text,
+.fact-counter .upper-title:before,
+.news-block .inner-box:hover .more-link a,
+.newsletter-form .form-group .newsletter-btn,
+.search-popup .search-form fieldset input[type="submit"],
+.cases-carousel-two .owl-dots .owl-dot.active span,
+.team-block .social-links li a:hover,
+.locations-section .map-box .location-point:before,
+.featured-block .inner-box:hover .more-link a,
+.sidebar .services-widget ul li:before,
+.sidebar .info-widget .widget-inner,
+.news-block-three .inner-box:hover .more-link a,
+.pagination li .current,
+.pagination li a:hover,
+.tagcloud a:hover,
+.news-block-three .lower-box .category a,
+.contact-section-two .social-links li a:hover,
+.testimonials-section-ex .owl-theme .owl-dots .owl-dot.active span,
+.bkr003 .pricing-6 .btn-style-one,
+.funfact-block-one .inner-box .icon-box:before,
+.team-ex-block-two .inner-box .content-box .social-links li a:hover,
+.feature-ex-section .single-item .inner-box:hover .icon-box,
+.service-ex-block .inner-box:hover .icon-box,
+.team-ex-block .inner-box .image-box .social-links li a:hover,
+.chooseus-ex-section .left-column .video-link a,
+.chooseus-ex-section .right-column .single-item .icon-box:before,
+.pricing-ex-section .content-box .tab-btns li:before,
+.image_block_5 .image-box .shape .shape-1,
+.information_siral,
+#appoinment_content .thm-spinner .ui-widget-header,
+#appoinment_content .thm-spinner.ui-slider .ui-slider-handle,
+.appoint_button,
+.bkr003 .pricing-5 .single-pricing-plan:hover .link a,
+.bkr003 .pricing-5 .sec-title-two:before,
+.bkr003 .pricing-6 .sec-title h2:after,
+.bkr003 .pricing-6 .price-block .inner-box .lower-box ul li:before,
+.bkr003 .pricing-8 .btn-style-two,
+.coupons-section .owl-dots .owl-dot:hover, .coupons-section .owl-dots .owl-dot.active,
+.services-price .table thead th,
+.bkr003 .team-1 .sec-title:after,
+.resume .info-box .title:after,
+.team-member-info-box .social-links-style1 li a:hover i,
+.ui-menu .ui-menu-item-wrapper:hover, .ui-menu .ui-menu-item-wrapper.ui-state-active,
+.btn-style-two .btn-title:before,
+.woocommerce .widget_price_filter .ui-slider .ui-slider-range,
+.woocommerce .widget_price_filter .ui-slider .ui-slider-handle,
+.share-it .post-share li,
+.default-form.add-comment-form .txt,
+.cases-section-ex .cases-carousel .owl-nav .owl-prev:hover, .cases-section-ex .cases-carousel .owl-nav .owl-next:hover,
+.image_block_7 .image-box .shape .shape-1,
+.case-block .content-box .link-box a:before,
+.cta-ex-section-two .cta-btn:before
+
+{
+	background: #2b7f36!important;
+	background-color:#2b7f36!important;
+}
+
+
+/*==============================================
+   Theme Border Color Css
+===============================================*/
+
+.main-menu .navigation > li:before,
+.header-top-one .language .lang-dropdown,
+.banner-carousel .content-box .sub-title:before,
+.sec-title .upper-text:before,
+.service-block .inner-box:before,
+.appointment-section .title h3:before,
+.main-footer .widget-title h4:before,
+.newsletter-form .form-group input[type="text"]:focus, .newsletter-form .form-group input[type="email"]:focus, .newsletter-form .form-group input[type="tel"]:focus,
+.team-block .social-links li a:hover,
+.locations-section .map-box .location-point,
+.bkr003 .testimonial-8 .testimonial-item-one:hover .text-area span,
+.inner-banner .d-text:before,
+.fun-facts-section .title-box h1:before,
+.sidebar .services-widget ul li:before,
+.sidebar-title h3:before,
+.pagination li .current,
+.pagination li a:hover,
+.tagcloud a:hover,
+.sidebar-widget.search-box .form-group input:focus,
+.contact-section-two .social-links li a:hover,
+.bkr003 .pricing-6 .btn-style-one,
+.bkr003 .pricing-5 .single-pricing-plan:hover .link a,
+.woocommerce .widget_price_filter .ui-slider .ui-slider-handle,
+.woocommerce .woocommerce-error, .woocommerce .woocommerce-info, .woocommerce .woocommerce-message,
+.default-form .form-group input[type="text"]:focus, .default-form .form-group input[type="email"]:focus, .default-form .form-group input[type="password"]:focus, .default-form .form-group input[type="tel"]:focus, .default-form .form-group input[type="url"]:focus, .default-form .form-group input[type="file"]:focus, .default-form .form-group input[type="number"]:focus, .default-form .form-group textarea:focus, .default-form .form-group select:focus, .form-group .ui-selectmenu-button.ui-button:focus, .form-group .ui-selectmenu-button.ui-button:active
+
+{
+    border-color: #2b7f36!important;  
+}
+
+/*==============================================
+   RGB
+===============================================*/
+
+.video-galler-outer-bg:before {
+    background-color: rgba(43,127,54,0.9)!important;
+}
+.main-slider .content h3:before{
+    background: rgba(43,127,54,0.4)!important;
+}
+
+
+
+
